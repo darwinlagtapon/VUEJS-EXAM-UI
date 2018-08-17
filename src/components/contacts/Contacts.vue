ce9c0901be26b04d54f0063c8a005b643969b27d
darwinlagtapon <darwin.lagtapon@gitlab.ewalletprovider.com>
2018-08-17 21:55:19 +0800

    first commit

---
+++ src/components/contacts/Contacts.vue
@@ -0,0 +1,38 @@
+<template src="./Contacts.html"></template>
+
+<script>
+
+export default {
+
+  name: 'Home',
+
+  data () {
+    return {
+      contacts: {},
+      formData: []
+    }
+  },
+
+  created () {
+  },
+
+  mounted () {
+    this.getContacts()
+  },
+
+  methods: {
+
+    getContacts () {
+      var url = '/contacts'
+      this.$http.get(url)
+        .then(response => {
+          this.contacts = response.data.data
+        })
+    }
+
+  }
+}
+
+</script>
+
+<style src="./Contacts.css"></style>
